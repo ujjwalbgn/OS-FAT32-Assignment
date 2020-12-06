e3eb56ef7b0e9613812eefb722946a322e78dbbf
ujjwalbgn <ubajagain@gmail.com>
2020-12-06 14:52:24 -0500

    working on it

--- mfs.c
+++ mfs.c
@@ -29,8 +29,109 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <stdio.h>
+#include <ctype.h>
 
-#define MAX_NUM_ARGUMENTS 3
+#define ATTR_READ_ONLY 0x01
+#define ATTR_HIDDEN 0x02
+#define ATTR_SYSTEM 0x04
+#define ATTR_VOLUME_ID 0x08
+#define ATTR_DIRECTORY 0x10
+#define ATTR_ARCHIVE 0x20
+
+//Structure for storing Directory 
+struct __attribute__ (  (__packed__)  ) DirectoryEntry
+{
+  char DIR_NAME[11];
+  uint8_t DIR_Attr;
+  uint8_t unsued1[8];
+  uint16_t DIR_FirstClusterHigh;
+  uint8_t unsued2[8];
+  uint16_t DIR_FirstClusterLow;
+  uint32_t DIR_FileSize;
+};
+
+
+struct DirectoryEntry dir[16];
+
+int16_t BPB_BytsPerSec;
+int8_t BPB_SecPerClus;
+int16_t BPB_RsvdSecCnt;
+int8_t BPB_NumFATs;
+int32_t BPB_FATz32;
+
+FILE *fp;
+int file_open =0;
+
+int32_t LABoOffset( int32_t sector)
+{
+  return( (sector - 2) * BPB_BytsPerSec) + (BPB_NumFATs * BPB_FATz32  * BPB_BytsPerSec) + (BPB_RsvdSecCnt * BPB_BytsPerSec) ;
+}
+
+int16_t NextLB (int32_t sector)
+{
+  uint32_t FATAddress = (BPB_BytsPerSec * BPB_RsvdSecCnt) + (sector * 4);
+  int16_t val;
+  fseek(fp, FATAddress, SEEK_SET);
+  fread(&val,2,1,fp);
+  return val;
+}
+
+int compare (char * userString, char * directoryString)
+{
+
+  char * dotdot = "..";
+
+  if (strncmp(dotdot,userString,2) == 0)
+  {
+    if(strncmp(userString,directoryString,2) == 0)
+    {
+      return 1;
+    }
+    return 0;
+  }
+
+  char IMG_Name[12];
+
+  strncpy(IMG_Name, directoryString,11);
+  IMG_Name[11] = '\0';
+
+  char input[11];
+  memset(input,0,11);
+  strncpy(input, userString, strlen(userString));
+
+  char expanded_name[12];
+  memset(expanded_name,' ',12);
+
+  char *token = strtok(input, ",");
+
+  strncpy(expanded_name, token, strlen(token));
+
+  token = strtok(NULL, ",");
+
+  if(token)
+  {
+    strncpy ((char *)(expanded_name+8),token,strlen(token));
+  }
+
+  expanded_name[11] = '/0';
+
+  int i;
+  for ( i =0; i < 11; i++)
+  {
+    expanded_name[i] = toupper(expanded_name[i]);
+  }
+
+  if(strncmp (expanded_name, IMG_Name,11) == 0)
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+#define MAX_NUM_ARGUMENTS 4
+#define NUM_ENTRIES 16
 
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
                                 // so we need to define what delimits our tokens.
